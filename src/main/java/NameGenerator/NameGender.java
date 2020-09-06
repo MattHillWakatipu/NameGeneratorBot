2b7bd17ca038f41f335f27f7be49803122b53969
Matt <matthillwakatipu@gmail.com>
2020-09-06 23:35:24 +1200

    Added gender percentages.

--- src/main/java/NameGenerator/NameGender.java
+++ src/main/java/NameGenerator/NameGender.java
@@ -2,6 +2,10 @@ package NameGenerator;
 
 public class NameGender {
 
+    static final double MASCULINE_PERCENTAGE = 0.4;
+    static final double FEMININE_PERCENTAGE = 0.4;
+    static final double UNISEX_PERCENTAGE = 0.2;
+
     enum Gender {
         MASCULINE,
         FEMININE,
