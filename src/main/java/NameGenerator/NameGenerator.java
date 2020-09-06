b8a73df349550dbf512d30537f4e7e1bb1a94f55
Matt <matthillwakatipu@gmail.com>
2020-09-06 23:36:07 +1200

    Added a testing region

--- src/main/java/NameGenerator/NameGenerator.java
+++ src/main/java/NameGenerator/NameGenerator.java
@@ -1,4 +1,21 @@
 package NameGenerator;
 
+import java.util.ArrayList;
+
+
+
+/**
+ *
+ */
 public class NameGenerator {
+
+    public NameGenerator() {
+        ArrayList<NameOrigin> demographics = new ArrayList<>();
+        demographics.add(new NameOrigin("scottish",1.0));
+        Region bastille = new Region("Bastille", demographics);
+    }
+
+    private void generateName() {
+
+    }
 }
